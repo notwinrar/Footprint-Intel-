16a798801ae4df0ac9a942cde95f42d21d845596
notwinrar <notwinrar@gmail.com>
2026-03-06 04:24:21 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
-/** @type {import('next').NextConfig} */const nextConfig = { reactStrictMode: true, images: { domains: ['lh3.googleusercontent.com'] }}module.exports = nextConfig
+**
+
+```js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['lh3.googleusercontent.com']
+  }
+}
+
+module.exports = nextConfig
+```
+
